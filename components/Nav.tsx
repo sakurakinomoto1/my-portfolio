1827549d91e4b8cca1e836f3993617a26da439c3
Fariha Adil <82678818+sakurakinomoto1@users.noreply.github.com>
2026-07-28 22:37:24 -0500

    added nav bar

---
+++ components/Nav.tsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <div>
+      <a href="#home"> Home </a>
+      <a href="#about"> About </a>
+      <a href="#skills"> Skills </a>
+      <a href="#projects"> Projects </a>
+      <a href="#contact"> Contact </a>
+    </div>
+  );
+}
