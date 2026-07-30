123bf78b44cc6452a74e62bf0bf79e8abb665f3a
Fariha Adil <82678818+sakurakinomoto1@users.noreply.github.com>
2026-07-29 19:36:23 -0500

    added styling to the nav bar and links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,28 @@
 export default function Nav() {
+  const navLinksStyle =
+    "font-semibold text-sm px-3 py-2 rounded-lg hover:bg-accent-tertiary";
   return (
-    <div>
-      <a href="#home"> Home </a>
-      <a href="#about"> About </a>
-      <a href="#skills"> Skills </a>
-      <a href="#projects"> Projects </a>
-      <a href="#contact"> Contact </a>
+    <div className="max-w-295 mx-auto px-7 bg-card py-2 border-2 border-dark rounded-2xl shadow-hard">
+      <a href="#home" className={navLinksStyle}>
+        {" "}
+        Home{" "}
+      </a>
+      <a href="#about" className={navLinksStyle}>
+        {" "}
+        About{" "}
+      </a>
+      <a href="#skills" className={navLinksStyle}>
+        {" "}
+        Skills{" "}
+      </a>
+      <a href="#projects" className={navLinksStyle}>
+        {" "}
+        Projects{" "}
+      </a>
+      <a href="#contact" className={navLinksStyle}>
+        {" "}
+        Contact{" "}
+      </a>
     </div>
   );
 }
