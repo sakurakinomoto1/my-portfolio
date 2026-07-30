e8d18500947ded95c35192bf60a9d1ea087afb9a
Fariha Adil <82678818+sakurakinomoto1@users.noreply.github.com>
2026-07-29 20:17:25 -0500

    added logo

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,11 @@ export default function Nav() {
   const navLinksStyle =
     "font-semibold text-sm px-3 py-2 rounded-lg hover:bg-accent-tertiary";
   return (
-    <div className="max-w-295 mx-auto px-7 bg-card py-2 border-2 border-dark rounded-2xl shadow-hard">
+    <div className="w-full max-w-295 mx-auto px-7 bg-card py-2 border-2 border-dark rounded-2xl shadow-hard flex items-center justify-between">
+      <div className="font-display font-bold text-lg flex items-center gap-2">
+        <span className="w-2.5 h-2.5  bg-accent-primary-deep rounded-full border border-dark"></span>
+        fariha ✦
+      </div>
       <a href="#home" className={navLinksStyle}>
         {" "}
         Home{" "}
