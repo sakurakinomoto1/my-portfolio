c4166de46a82f54df71b4ebf4c8f2a2e55acb322
Fariha Adil <82678818+sakurakinomoto1@users.noreply.github.com>
2026-07-29 23:42:47 -0500

    adjusted text size and spacing

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,10 @@
 export default function Nav() {
   const navLinksStyle =
-    "font-semibold text-sm px-3 py-2 rounded-lg hover:bg-accent-tertiary";
+    "font-semibold text-[15px] px-3 py-2 rounded-lg hover:bg-accent-tertiary";
   return (
     <nav className="pt-4">
-      <div className="w-full max-w-295 mx-auto px-7 bg-card py-2 border-2 border-dark rounded-2xl shadow-hard flex items-center justify-between">
-        <div className="font-display font-bold text-lg flex items-center gap-2">
+      <div className="w-full max-w-295 mx-auto px-7 bg-card py-2.5 border-2 border-dark rounded-2xl shadow-hard flex items-center justify-between">
+        <div className="font-display font-bold text-lg flex items-center gap-2 pt-0.5">
           <span className="w-2.5 h-2.5  bg-accent-primary-deep rounded-full border-[1.7px] border-dark"></span>
           fariha ✦
         </div>
@@ -30,7 +30,7 @@ export default function Nav() {
 
         <a
           href="#contact"
-          className="font-display font-bold text-sm px-5.5 py-3 border-2 border-dark rounded-xl bg-accent-primary shadow-hard-med transition duration-120 hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-hard-xsm active:translate-x-1 active:translate-y-1 active:shadow-none"
+          className="inline-flex items-center justify-center font-display font-bold text-[15px] px-5.5 pt-3.5 pb-3 border-2 border-dark rounded-xl bg-accent-primary shadow-hard-med transition duration-120 hover:translate-x-0.5 hover:translate-y-0.5 hover:shadow-hard-xsm active:translate-x-1 active:translate-y-1 active:shadow-none"
         >
           Get in touch
         </a>
