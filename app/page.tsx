8881544b48553dc98c6b978c2ecb987ddbd65b75
Fariha Adil <82678818+sakurakinomoto1@users.noreply.github.com>
2026-08-01 11:38:56 -0500

    imported and rendered hero component on page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Nav from "@/components/Nav";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <Nav />
+    <>
+      <Nav />
+      <Hero />
+    </>
   );
 }
